a7ac9bacfdf7eed60b2e42c1442ca4922759706f
hemant5454 <hemant54mishra@gmail.com>
2018-07-27 22:21:37 +0530

    Added a prime function

--- prime_1.py
+++ prime_1.py
@@ -1,5 +1,10 @@
 def prime(n):
-    return n
+    if(n==1):
+        return False
+    for i in range(2,n//2):
+        if(n%i==0):
+            return False
+    return True
     #Write code of this function
 n=int(input())
 print(prime(n))
